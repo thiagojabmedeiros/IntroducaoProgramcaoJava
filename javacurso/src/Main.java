eb299c22fc7f00f9acfb55c6de2c6b58a457747d
Thiago Medeiros <thiagojabm@gmail.com>
2025-09-14 21:27:01 -0300

    update

--- javacurso/src/Main.java
+++ javacurso/src/Main.java
@@ -1,5 +1,8 @@
 package src;
 
 public class Main {
-    
+    public static void Main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println("This is my first code in java and i'm so glad to share it with you!");
+    }
 }
